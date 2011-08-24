f0be6a01023efc45b9777949d84949bb9581bc65
Francisco Baptista <francisco@tomboa.net>
2011-08-25 00:39:57 +0100

    test product movie1

---
+++ assets/tests/ProductMovie1TestCase.php
@@ -0,0 +1,31 @@
+<?php
+require_once('TestSuite.php');
+
+/**
+ * Test Products
+ */
+class ProductMovie1TestCase extends TestSuite
+{
+	/**
+	 * Test product code
+	 * 
+	 * @access public
+	 * @asserts product code is type string
+	 */
+	public function testMovie1ProductCode()
+	{
+		$this->assertInternalType(PHPUnit_Framework_Constraint_IsType::TYPE_STRING, $this->_product->getCode());
+	}
+
+	/**
+	 * Test if product has an Addon
+	 * 
+	 * @access public
+	 * @asserts product Addon array not empty
+	 */ 
+	public function testMovie1ProductDoesHaveAddOn()
+	{
+		$addon = $this->_product->countAddons();
+		$this->assertTrue( ! empty($addon));
+	}
+}
