5115139511455f7c1c76823f834f32c1111e2d2a
Francisco Baptista <francisco@tomboa.net>
2011-08-24 23:56:08 +0100

    style presentation and other minor improvements

--- assets/tests/TestSuite.php
+++ assets/tests/TestSuite.php
@@ -8,10 +8,24 @@ require_once('../../src/bootstrap.php');
  */
 class TestSuite extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * Product codes
+	 * @var array 
+	 */
 	protected $_products_codes = array('MOVIE1', 'KIDS', 'NEWS');
 	
+	/**
+	 * Product object
+	 * 
+	 * @var object 
+	 */
 	protected $_product;
 	
+	/**
+	 * Basket object
+	 * 
+	 * @var object 
+	 */
 	protected $_basket;
 	
 	/**
@@ -28,11 +42,12 @@ class TestSuite extends PHPUnit_Framework_TestCase
 	}
 	
 	/**
-	 * TearDown
+	 * TearDown {@link Basket} and {@link Product} objects
 	 */
 	function tearDown()
 	{
         unset($this->_basket);
+		
         unset($this->_product);
     }
 }
