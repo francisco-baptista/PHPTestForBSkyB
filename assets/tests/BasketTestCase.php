53b91b5b11bd778c2f669fc5915e5844fa0000dc
Francisco Baptista <francisco@tomboa.net>
2011-08-25 00:50:00 +0100

    minor changes

--- assets/tests/BasketTestCase.php
+++ assets/tests/BasketTestCase.php
@@ -6,17 +6,22 @@ require_once('TestSuite.php');
  */
 class BasketTestCase extends TestSuite
 {
-	
 	/**
 	 * Check if {@link $_basket} is an instance of {@link Basket} class
+	 * 
+	 * @access public
+	 * @asserts object is instence of {@link Basket}
 	 */
-	public function testRealBasket()
+	public function testIsInstanceOfBasket()
 	{
 		$this->assertInstanceOf('Basket', $this->_basket);;
 	}
 	
 	/**
 	 * Test the basket is empty
+	 * 
+	 * @access public
+	 * @aserts basket is empty
 	 */
 	public function testEmptyBasket()
 	{
@@ -26,13 +31,27 @@ class BasketTestCase extends TestSuite
 	/**
 	 * Test of the product added to the basket can be found in the basket
 	 * 
+	 * @access public
+	 * @asserts if product can be found in basket
 	 */
-	public function testAddNewProductToBasket()
+	public function testProductCanBeFoundInBasket()
 	{
 		// add product to basket
 		$this->_basket->addItem($this->_product);
 		
 		// aserts product can be found in the basket
-		$this->assertEquals(true, in_array($this->_product->getCode(), $this->_basket->getItems()->getCodes()));
+		$this->assertEquals(TRUE, in_array($this->_product->getCode(), $this->_basket->getItems()->getCodes()));
+	}
+	
+	/**
+	 * Test if random product code is in
+	 * 
+	 * @access public
+	 * @asserts a random product is in the basket
+	 */
+	public function testRandomProductIsInBasket()
+	{
+		// aserts product can be found in the basket
+		$this->assertFalse(in_array('RANDOM', $this->_basket->getItems()->getCodes()));
 	}
 }
